4aec09e4d88dc1976d08e3785c58e170d9166d34
MOUAD_CHEMLAL <chemlalmouad2002@gmail.com>
2024-05-27 10:36:23 +0200

    v1

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::create(['name' => 'Administrator']);
+        $permission = Permission::create(['name' => 'manage tasks']);
+        $permission->assignRole($adminRole);
+
+        $adminUser = User::factory()->create([
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('SecurePassword')
+        ]);
+        $adminUser->assignRole('Administrator');
+    }
+}
